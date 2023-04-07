cc917d7bd8ccd0de94de4bedab6bc7e2d753ca66
loadedice <loadedice@users.noreply.github.com>
2023-04-07 15:38:59 +1000

    fix unsued app warning

--- src/ui.rs
+++ src/ui.rs
@@ -9,7 +9,7 @@ use tui::{
 use crate::app::App;
 
 /// Renders the user interface widgets.
-pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
+pub fn render<B: Backend>(_app: &mut App, frame: &mut Frame<'_, B>) {
     // This is where you add new widgets.
     // See the following resources:
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
