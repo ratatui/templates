7c9f09fe5ea20dde4a7cbbc56588dad670ee249a
loadedice <loadedice@users.noreply.github.com>
2023-04-07 15:52:29 +1000

    add a demo counter to show how to use app in the render function

--- src/app.rs
+++ src/app.rs
@@ -8,11 +8,16 @@ pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 pub struct App {
     /// Is the application running?
     pub running: bool,
+    /// counter
+    pub counter: u8,
 }
 
 impl Default for App {
     fn default() -> Self {
-        Self { running: true }
+        Self {
+            running: true,
+            counter: 0,
+        }
     }
 }
 
@@ -25,8 +30,20 @@ impl App {
     /// Handles the tick event of the terminal.
     pub fn tick(&self) {}
 
-    /// Set running to false to quit the applcation.
+    /// Set running to false to quit the application.
     pub fn quit(&mut self) {
         self.running = false;
+    }
+
+    pub fn increment_counter(&mut self) {
+        if let Some(res) = self.counter.checked_add(1) {
+            self.counter = res;
+        }
+    }
+
+    pub fn decrement_counter(&mut self) {
+        if let Some(res) = self.counter.checked_sub(1) {
+            self.counter = res;
+        }
     }
 }
--- src/handler.rs
+++ src/handler.rs
@@ -14,6 +14,13 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
                 app.quit();
             }
         }
+        // Counter handlers
+        KeyCode::Right => {
+            app.increment_counter();
+        }
+        KeyCode::Left => {
+            app.decrement_counter();
+        }
         // Other handlers you could add here.
         _ => {}
     }
