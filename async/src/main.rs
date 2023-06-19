a55352b63db857dfb09b49eb37ccac03543adb15
Dheepak Krishnamurthy <me@kdheepak.com>
2023-06-19 05:17:03 -0400

    feat: Add build.rs with shadow ✨

--- async/src/main.rs
+++ async/src/main.rs
@@ -1,12 +1,27 @@
 use anyhow::Result;
 use clap::Parser;
+// use clap_complete::{generate, Shell as CompletionShell};
 use colored::Colorize;
 use ratatui_template::{app::App, logging::initialize_logging, tui::Tui, utils::initialize_panic_handler};
+use shadow_rs::{formatcp, shadow};
 use tracing::error;
 
-/// Ratatui Template TUI
+shadow!(build);
+
+const VERSION_INFO: &str = formatcp!(
+  r#"{}
+commit_hash: {}
+build_time: {}
+build_env: {}, {}"#,
+  build::PKG_VERSION,
+  build::SHORT_COMMIT,
+  build::BUILD_TIME,
+  build::RUST_VERSION,
+  build::RUST_CHANNEL
+);
+
 #[derive(Parser, Debug)]
-#[command(author, version, about)]
+#[command(author=clap::crate_authors!(), version=VERSION_INFO, about = "ratatui template with crossterm and tokio")]
 struct Args {
   /// The tick rate to use
   #[arg(short, long, default_value_t = 50)]
