0848f11eb8792238ad6850e06e3f5f48deadf72c
Dheepak Krishnamurthy <me@kdheepak.com>
2023-09-28 08:52:26 -0400

    feat: Update version string ✨

--- async/ratatui-counter/src/cli.rs
+++ async/ratatui-counter/src/cli.rs
@@ -2,8 +2,10 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
+use crate::utils::version;
+
 #[derive(Parser, Debug)]
-#[command(author, version, about)]
+#[command(author, version = version(), about)]
 pub struct Cli {
   #[arg(short, long, value_name = "FLOAT", help = "Tick rate, i.e. number of ticks per second", default_value_t = 1.0)]
   pub tick_rate: f64,
