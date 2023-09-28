e9afa9688c062a6927afdc569f35e7661fd70078
Dheepak Krishnamurthy <me@kdheepak.com>
2023-09-28 08:36:01 -0400

    feat: Make cli.rs use short and long args ✨

--- async/template/src/cli.rs
+++ async/template/src/cli.rs
@@ -5,9 +5,15 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 pub struct Cli {
-  #[arg(value_name = "FLOAT", help = "Tick rate, i.e. number of ticks per second", default_value_t = 1.0)]
+  #[arg(short, long, value_name = "FLOAT", help = "Tick rate, i.e. number of ticks per second", default_value_t = 1.0)]
   pub tick_rate: f64,
 
-  #[arg(value_name = "FLOAT", help = "Frame rate, i.e. number of frames per second", default_value_t = 60.0)]
+  #[arg(
+    short,
+    long,
+    value_name = "FLOAT",
+    help = "Frame rate, i.e. number of frames per second",
+    default_value_t = 60.0
+  )]
   pub frame_rate: f64,
 }
