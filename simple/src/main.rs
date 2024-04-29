418ba7b8882e7a0a06c61fe25c95ce45e270dc88
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-04-30 01:55:59 +0300

    chore: revert changes

--- simple/src/main.rs
+++ simple/src/main.rs
@@ -1,10 +1,10 @@
-use ratatui::backend::CrosstermBackend;
-use ratatui::Terminal;
-use std::io;
 use {{crate_name}}::app::{App, AppResult};
 use {{crate_name}}::event::{Event, EventHandler};
 use {{crate_name}}::handler::handle_key_events;
 use {{crate_name}}::tui::Tui;
+use std::io;
+use ratatui::backend::CrosstermBackend;
+use ratatui::Terminal;
 
 fn main() -> AppResult<()> {
     // Create an application.
