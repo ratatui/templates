8bdffff9e3adf038a489d8fa55e54eb6be39f5f7
Josh McKinney <joshka@users.noreply.github.com>
2024-07-14 11:25:19 -0700

    refactor: use comments instead of attribute to document cli params

--- component/template/src/cli.rs
+++ component/template/src/cli.rs
@@ -5,21 +5,11 @@ use crate::utils::version;
 #[derive(Parser, Debug)]
 #[command(author, version = version(), about)]
 pub struct Cli {
-    #[arg(
-        short,
-        long,
-        value_name = "FLOAT",
-        help = "Tick rate, i.e. number of ticks per second",
-        default_value_t = 4.0
-    )]
+    /// Tick rate, i.e. number of ticks per second
+    #[arg(short, long, value_name = "FLOAT", default_value_t = 4.0)]
     pub tick_rate: f64,
 
-    #[arg(
-        short,
-        long,
-        value_name = "FLOAT",
-        help = "Frame rate, i.e. number of frames per second",
-        default_value_t = 60.0
-    )]
+    /// Frame rate, i.e. number of frames per second
+    #[arg(short, long, value_name = "FLOAT", default_value_t = 60.0)]
     pub frame_rate: f64,
 }
