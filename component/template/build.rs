dde7165697f6e37c5d61cd5205a515cf94f1fa1b
Josh McKinney <joshka@users.noreply.github.com>
2024-08-23 12:05:15 -0700

    Fix vergen to use gix (#70)

--- component/template/build.rs
+++ component/template/build.rs
@@ -1,12 +1,13 @@
 use anyhow::Result;
-use vergen::{BuildBuilder, Emitter};
-use vergen_gix::GixBuilder;
+use vergen_gix::{BuildBuilder, CargoBuilder, Emitter, GixBuilder};
 
 fn main() -> Result<()> {
     let build = BuildBuilder::all_build()?;
     let gix = GixBuilder::all_git()?;
+    let cargo = CargoBuilder::all_cargo()?;
     Emitter::default()
         .add_instructions(&build)?
         .add_instructions(&gix)?
+        .add_instructions(&cargo)?
         .emit()
 }
