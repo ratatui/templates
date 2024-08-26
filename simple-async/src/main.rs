f55a68fb0fc91ffc0de30d274262fc2c1d16c86e
pythops <contact@pythops.com>
2024-08-26 22:13:36 +0200

    fix: use stdout when initializing the terminal

--- simple-async/src/main.rs
+++ simple-async/src/main.rs
@@ -21,7 +21,7 @@ async fn main() -> AppResult<()> {
     let mut app = App::new();
 
     // Initialize the terminal user interface.
-    let backend = CrosstermBackend::new(io::stderr());
+    let backend = CrosstermBackend::new(io::stdout());
     let terminal = Terminal::new(backend)?;
     let events = EventHandler::new(250);
     let mut tui = Tui::new(terminal, events);
