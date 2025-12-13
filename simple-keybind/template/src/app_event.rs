87b9c5ed10a715a442ca2e68255a9d2eb95777da
Antonio Yang <yanganto@gmail.com>
2025-12-13 08:33:23 +0800

    docs

--- simple-keybind/template/src/app_event.rs
+++ simple-keybind/template/src/app_event.rs
@@ -1,5 +1,13 @@
 use crossterm_keybind::KeyBind;
 
+// NOTE
+// The KeyBindTrait will be impletemeted, so you can use the methods to generate and load a
+// keybind config, and users can easily customize from a config.
+// https://docs.rs/crossterm-keybind/latest/crossterm_keybind/trait.KeyBindTrait.html
+// 
+// Example using case: 
+// use `to_toml_example(path)` to a file, after user customized it, it can loaded by
+// `init_and_load(path)` when app starting.
 #[derive(KeyBind)]
 pub enum AppKeyEvent {
     /// The app will be closed with following key bindings
